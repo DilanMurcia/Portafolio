203fde49620e219ef8d73e29fb5bfb5eb5dc7cff
Dilan Albert Murcia Parra <dilanalbertmurcia@gmail.com>
2025-06-30 20:04:21 -0500

    Actualizo dependencias

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,20 +2,18 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
 		title: z.string(),
     seoTitle: z.string().optional(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string()).optional(),
 		coverImage: image()
-      .refine((img) => img.width >= 960, {
-        message: 'Cover image must be at least 960 pixels wide!'
-      })
       .optional()
+     // .refine((img) => !img || img.width >= 960, {
+    //    message: 'Cover image must be at least 960 pixels wide if provided!'
+     // })
 	}),
 });
 
